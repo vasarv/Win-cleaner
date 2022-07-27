0473e5d1c1da509d2dc816469eea0966b44e2332
vasarv <76836836+vasarv@users.noreply.github.com>
2022-07-27 10:21:43 +0300

    Create Win-Cleaner-NoReboot.bat

---
+++ Win-Cleaner-NoReboot.bat
@@ -0,0 +1,9 @@
+WSReset.exe
+TASKKILL /F /IM WinStore.App.exe
+ipconfig /flushdns
+dism /online /cleanup-image /startcomponentcleanup /resetbase
+pushd "C:\Windows\Temp" && (rd /s /q "C:\Windows\Temp" 2>nul & popd)
+pushd "C:\Windows\SoftwareDistribution\Download" && (rd /s /q "C:\Windows\SoftwareDistribution\Download" 2>nul & popd)
+pushd "C:\Windows\Prefetch" && (rd /s /q "C:\Windows\Prefetch" 2>nul & popd)
+pushd "%temp%" && (rd /s /q "%temp%" 2>nul & popd)
+powershell.exe -command Clear-RecycleBin
